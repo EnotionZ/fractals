6f6780b747a536a10a0c8f8982c19f7ea5d3c580
Dominick Pham <dominick.pham@gmail.com>
2011-03-13 20:46:25 -0400

    added mousescroll event for size and mousemove event for location

--- ag.js
+++ ag.js
@@ -1,28 +1,87 @@
-var cycles = 7,
-	ctx = document.getElementById('ag').getContext('2d'),
-	ag = function(){
+var radius = 10,
+	canvas = document.getElementById('ag'),
+	ctx = canvas.getContext('2d'),
+	
+	
+	
+	wheelCatch = function(e) {
+        var delta = 0;
+		if(!e) e = window.event;
+		
+        if (event.wheelDelta) { /* IE/Opera. */
+                delta = event.wheelDelta/120;
+                if (window.opera)
+                        delta = -delta;
+        } else if (e.detail) { /** Mozilla case. */
+                /** In Mozilla, sign of delta is different than in IE.
+                 * Also, delta is multiple of 3. */
+                delta = -e.detail/3;
+        }
+        /** If delta is nonzero, handle it.
+         * Basically, delta is now positive if wheel was scrolled up,
+         * and negative, if wheel was scrolled down.
+         */
+        if (delta) mouseScroll(delta, e);
+        /** Pre default actions caused by mouse wheel.
+         * That might be ugly, but we handle scrolls somehow
+         * anyway, so don't bother here..
+         */
+        if (e.preDefault) e.preDefault();
+		e.returnValue = false;	
+	},
+	mouseScroll = function(delta, e) {
+		if( radius < 3 && delta < 0 || radius > ag.prototype.WIDTH/2 - 40 && delta > 0) return;
+		if(delta < 0) radius--;
+		else radius++;
+		
+		mouseMove(e);
+	},
+	mouseMove = function(e) {
+		var x = e.clientX - 38,
+		y = e.clientY - 38,
+		c = ag.prototype.browserToGraph({x:x,y:y});
+		x = c.x;
+		y = c.y;
+
+		//don't show anything outside of the circle
+		if(Math.pow(x,2) + Math.pow(y,2)  < Math.pow(ag.prototype.WIDTH/2 - radius,2))
+			new ag(x,y);
+	},
+	
+	
+	
+	ag = function(x, y){
+		
 		var bigR = this.WIDTH/2,
-			r = 20,
-			x = 1,
-			y = 1;
-			
+			r = radius,
+			x = x || 0,
+			y = y || 0;
+		
+		x = (x==0 ? 1 : x);
+		y = (y==0 ? 1 : y);
+		
 		// big circle
-		this.drawCircle(0,0,bigR);
+		this.drawCircle(0,0,bigR, true);
 		
-		// first inner soddy circle
-		this.drawCircle(x,y,r);
+		try {
+			// first inner soddy circle
+			this.drawCircle(x,y,r);
+			//console.log(x + '|' + y);
+			
+			// top circle
+			var h = Math.sqrt(Math.pow(x,2) + Math.pow(y,2)),
+				newR = (bigR-(h + r))/2,
+				newH = h + r + newR,
+				theta = Math.atan(y/x),
+				newX = newH*Math.cos(theta) * ( x<0 ? -1 : 1),
+				newY = newH*Math.sin( theta) * (x<0 ? -1 : 1);
+			this.drawCircle(newX,newY,newR);
+			
+		} catch(e) {
 		
-		// top circle
-		var h = Math.sqrt(Math.pow(x,2) + Math.pow(y,2)),
-			newR = (bigR-(h + r))/2,
-			newH = h + r + newR,
-			theta = 2*Math.PI*3/4 +  Math.atan(y/x),
-			newX = newH*Math.cos(theta),
-			newY = newH*Math.sin(-theta);
-		this.drawCircle(newX,newY,newR);
+		}
 		
-		
-		
+		this.drawGrid();
 		return false;
 	};
 
@@ -30,20 +89,44 @@ ag.prototype = {
 	WIDTH : 500,
 	HEIGHT : 500,
 	
-	normalize: function(coord) {
-		coord.x += this.WIDTH/2;
-		coord.y = this.HEIGHT/2 - coord.y;
-		return {x: coord.x, y: coord.y};
-	},
-	
-	drawCircle : function(x, y, r){
+	drawGrid: function(){
 		ctx.beginPath();
-		var coord = this.normalize({x: x, y: y});
-		
-		// arc(x, y, radius, startAngle, endAngle, anticlockwise)
-		ctx.arc(coord.x, coord.y, r, 0, Math.PI*2, true);
+		ctx.moveTo(this.WIDTH/2, 0);
+		ctx.lineTo(this.WIDTH/2, this.HEIGHT);
+		ctx.moveTo(0, this.HEIGHT/2);
+		ctx.lineTo(this.WIDTH, this.HEIGHT/2);
 		ctx.stroke();
 		ctx.closePath();
+	},
+	
+	browserToGraph: function(coord) {
+		coord.x -= this.WIDTH/2;
+		coord.y = this.HEIGHT/2 - coord.y;
+		return coord;
+	},
+	
+	graphToBrowser: function(coord) {
+		coord.x += this.WIDTH/2;
+		coord.y = this.HEIGHT/2 - coord.y;
+		return coord;
+	},
+	
+	drawCircle : function(x, y, r, fill){
+		ctx.beginPath();
+		if(fill) ctx.fillStyle = "white";
+		ctx.lineWidth = 2;
+		var coord = this.graphToBrowser({x: x, y: y});
+		
+		// arc(x, y, radius, startAngle, endAngle, anticlockwise)
+		ctx.arc(coord.x, coord.y, r, 0, Math.PI*2, false);
+		ctx.stroke();
+		ctx.fill();
+		ctx.closePath();
 	}
+	
 }
-a = new ag();
+
+canvas.addEventListener('mousemove', function(e){ mouseMove(e); }, false);
+
+canvas.addEventListener('DOMMouseScroll', wheelCatch, false);
+window.onmousewheel = document.onmousewheel = wheelCatch;
