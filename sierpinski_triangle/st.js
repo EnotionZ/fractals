c0eb8448dcf31654485ac98c3916d58976f24738
Dominick Pham <dominick.pham@gmail.com>
2011-06-09 20:07:15 -0400

    wow this used to be pretty bad code. Updating with better design. Canvas also now expands to parent's width

--- sierpinski_triangle/st.js
+++ sierpinski_triangle/st.js
@@ -1,60 +1,63 @@
-var 
-	canvas = document.getElementById('ag'),
-	ctx = canvas.getContext('2d'),
-	mouseMove = function(e) {
-		var x = e.clientX - 38,
-			y = e.clientY - 38,
-			x = Math.abs(st.prototype.screenToX(x));
+!function(){
+	
+	var 
+		canvas = document.getElementById('ag'),
+		width = canvas.parentElement.offsetWidth - 20,	// minus some padding
+		height = width*Math.sqrt(3)/2,
+		ctx = canvas.getContext('2d'),
+		mouseMove = function(e) {
+			var x = e.clientX - canvas.offsetLeft,
+				y = e.clientY - canvas.offsetTop,
+				x = Math.abs(screenToX(x));
 			
-		// draw initial upright triangle
-		st.prototype.drawTriangle(0,st.prototype.HEIGHT,st.prototype.WIDTH, true);
-		new st(x,st.prototype.HEIGHT/2, 0, st.prototype.WIDTH/2, 0);
-	
-	},
-	
-	st = function(x, y, lvl, maxw, xOffset ){
-	
-		if (lvl == 8) return; 
+			
+			drawFirstTriangle();
+			draw(x,height/2, 0, width/2, 0);
+		},
+		xToScreen = function(x) { return x+width/2; },
+		screenToX = function(x) { return x-width/2; },
+		drawFirstTriangle = function() { drawTriangle(0, height, width, true); },
+		drawTriangle = function(x, y, width, upright) {
+
+			var x = xToScreen(x),
+				height = width*Math.sqrt(3)/2;
+			if(upright) height = -height;
+
+			ctx.beginPath()
+			ctx.moveTo(x-width/2, y);
+			ctx.lineTo(x+width/2, y);
+			ctx.lineTo(x, y + height);
+			ctx.lineTo(x-width/2,y);
+			ctx.closePath();
+			if(upright) {
+				ctx.fillStyle = "white";
+				ctx.fill();
+			}
+			ctx.stroke();
+		},
 		
-		if(x*2 <= maxw){
-			// drawing in progress
-			this.drawTriangle(xOffset, y, x*2);
-		} else {
-			//draw base, then anim draw
-			var nx= x - maxw/2,
-				maxh = maxw*Math.sqrt(3)/2;
-			this.drawTriangle(xOffset, y, maxw);
-			new st(nx, y- maxh/2, lvl+1, maxw/2, xOffset);
-			new st(nx, y+ maxh/2, lvl+1, maxw/2, xOffset+maxw/2);
-			new st(nx, y+ maxh/2, lvl+1, maxw/2, xOffset-maxw/2);
-		}
-	};
-
-st.prototype = {
-	WIDTH: 800,
-	HEIGHT: 800*Math.sqrt(3)/2,
+		draw = function(x, y, lvl, maxw, xOffset ){
 	
-	xToScreen: function(x) { return x+this.WIDTH/2; },
-	screenToX: function(x) { return x-this.WIDTH/2; },
-	drawTriangle: function(x, y, width, upright) {
+			if (lvl == 8) return; 
+		
+			if(x*2 <= maxw){
+				drawTriangle(xOffset, y, x*2);
+			} else {
+				//draw base, then draw necessary subsequents
+				var nx= x - maxw/2,
+					maxh = maxw*Math.sqrt(3)/2;
+				drawTriangle(xOffset, y, maxw);
+				draw(nx, y- maxh/2, lvl+1, maxw/2, xOffset);
+				draw(nx, y+ maxh/2, lvl+1, maxw/2, xOffset+maxw/2);
+				draw(nx, y+ maxh/2, lvl+1, maxw/2, xOffset-maxw/2);
+			}
+		};
 
-		var x = st.prototype.xToScreen(x),
-			height = width*Math.sqrt(3)/2;
-		if(upright) height = -height;
+	canvas.setAttribute('width', width);
+	canvas.setAttribute('height', height);
 
-		ctx.beginPath()
-		ctx.moveTo(x-width/2, y);
-		ctx.lineTo(x+width/2, y);
-		ctx.lineTo(x, y + height);
-		ctx.lineTo(x-width/2,y);
-		ctx.closePath();
-		if(upright) {
-			ctx.fillStyle = "white";
-			ctx.fill();
-		}
-		ctx.stroke();
-	}
+	document.addEventListener('mousemove', function(e){ mouseMove(e); }, false);
+	
+	drawFirstTriangle();
 
-};
-document.addEventListener('mousemove', function(e){ mouseMove(e); }, false);
-st.prototype.drawTriangle(0,st.prototype.HEIGHT,st.prototype.WIDTH, true);
+}();
