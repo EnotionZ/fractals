97dc0aee137bf3158198426647a77a9846d0e510
Dominick Pham <dominick.pham@gmail.com>
2011-07-25 20:01:21 -0400

    apollonian gasket with outer soddy edges curvatures, done

--- apollonian_gasket/ag.js
+++ apollonian_gasket/ag.js
@@ -22,10 +22,10 @@
 	},
 	
 	// gets x and y of soddy given c1, c2, and r
-	getPoint = function(c1, c2, r, lvl) {
+	getPoint = function(c1, c2, r, q, lvl) {
 		var
 		
-		x, y, hyp, theta,
+		x, y, hyp, theta, tc12,
 		
 		a = c1.r+c2.r,
 		b = c1.r+r,
@@ -45,24 +45,15 @@
 			y = -Math.sqrt(Math.pow(b,2) - Math.pow(x-c1.x, 2)) + c1.y;	
 		} else {
 			hyp = Math.sqrt(Math.pow(opp,2) + Math.pow(adj,2));
-			theta = Math.acos((Math.pow(hyp,2) + Math.pow(adj,2) - Math.pow(opp,2))/(2*hyp*adj));
+			tc12 = Math.acos((Math.pow(hyp,2) + Math.pow(adj,2) - Math.pow(opp,2))/(2*hyp*adj));
 
 
 			if(c2.x<0) {
-				// left half
-				theta = -B - theta;
-				
-				x = c2.x + (adj < 0 ? c*Math.cos(theta) : -c*Math.cos(theta));
-				y = c2.y + (opp < 0 ? -c*Math.sin(theta) : c*Math.sin(theta));
+				theta = (q==='tl' || q==='tr' ? -B : B) + (opp<0 ? tc12 : -tc12);
 				x = c2.x + c*Math.cos(theta);
 				y = c2.y - c*Math.sin(theta);
 			} else {
-				// right half
-				theta =  B - theta;
-				//if(lvl == 2 && opp < 0) theta = theta - 2*B;
-				
-				x = c2.x - (adj < 0 ? c*Math.cos(theta) : -c*Math.cos(theta));
-				y = c2.y + (opp < 0 ? -c*Math.sin(theta) : c*Math.sin(theta));
+				theta = (q==='tl' || q==='tr' ? -B : B) + (opp<0 ? - tc12 : tc12);
 				x = c2.x + c*Math.cos(theta);
 				y = c2.y + c*Math.sin(theta);
 			}
@@ -72,7 +63,7 @@
 		return { x : x, y : y }
 	},
 	
-	getSoddyEdge = function(c1, c2, lvl) {
+	getSoddyEdge = function(c1, c2, q, lvl) {
 		var 
 
 		numerator = c1.r*c2.r*r0,
@@ -80,7 +71,7 @@
 		denom2 = 2*Math.sqrt(c1.r*c2.r*r0*(c1.r+c2.r+r0)),
 
 		edgeC = { r: numerator/(denom1-denom2) },
-		point = getPoint(c1, c2, edgeC.r, lvl);
+		point = getPoint(c1, c2, edgeC.r, q, lvl);
 
 		edgeC.x = point.x;
 		edgeC.y = point.y;
@@ -108,15 +99,15 @@
 		return { cTop: c1, cBottom: c2, cLeft: edgeCL, cRight: edgeCR };
 	},
 	
-	ag = function(c1, c2, c3, lvl){
+	ag = function(c1, c2, c3, q, lvl){
 		lvl++;
 		
-		if(lvl === 8) return;
+		if(lvl === 7) return;
 		if(c3 === 'edge') {
-			var c = getSoddyEdge(c1, c2, lvl)
+			var c = getSoddyEdge(c1, c2, q, lvl)
 			drawCurvature(c);
-			ag(c1,c,'edge',lvl);
-			ag(c,c2,'edge',lvl);
+			ag(c1,c,'edge', q, lvl);
+			ag(c,c2,'edge', q, lvl);
 		}
 		return false;
 	};
@@ -131,10 +122,10 @@
 		//don't show anything outside of the circle
 		if(Math.pow(mx,2) + Math.pow(my,2)  < Math.pow(WIDTH/2 ,2)) {
 			s = drawSetup(mx,my);
-			ag(s.cTop, s.cLeft, 'edge', 0);
-			ag(s.cTop, s.cRight, 'edge', 0);
-			ag(s.cBottom, s.cLeft, 'edge', 0);
-			ag(s.cBottom, s.cRight, 'edge', 0);
+			ag(s.cTop, s.cLeft, 'edge', 'tl', 0);
+			ag(s.cTop, s.cRight, 'edge', 'tr', 0);
+			ag(s.cBottom, s.cLeft, 'edge', 'bl', 0);
+			ag(s.cBottom, s.cRight, 'edge', 'br', 0);
 		}
 	}, false);
 
